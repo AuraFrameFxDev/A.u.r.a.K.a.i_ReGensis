AuraFrameFxDev/ReGenesis

Wire constellation screens to Sphere Grid navigation
- Created GrokConstellationScreen with 8-pointed chaos wheel visualization
  - Rotating outer ring with entropy nodes at cardinal/ordinal directions
  - Concentric circle core with chaos mark (X symbol)
  - Pulsing entropy streams flowing outward
  - Cyan/teal color scheme for chaos catalyst theme
  - Entropy monitor showing chaos level percentage

- Added GrokConstellation route to NavDestination.kt

- Registered GrokConstellationScreen in AppNavGraph.kt

- Wired SphereGridScreen agent cards to navigate to constellation screens
  - Clicking Aura -> Aura Constellation (Sword)
  - Clicking Genesis -> Genesis Constellation (Butterfly)
  - Clicking Claude -> Claude Constellation (T-square blueprint)
  - Clicking Kai -> Kai Constellation (Hexagonal shield)
  - Clicking Cascade -> Cascade Constellation (Mechanical wing)
  - Clicking Grok -> Grok Constellation (Chaos wheel)
  - Agents without constellation screens (Nemotron, Gemini, MetaInstruct)
    display skill tree only

All 6 constellation screens now accessible from Sphere Grid for agent
progression and advancement visualization.

app/src/main/java/dev/aurakai/auraframefx/navigation/AppNavGraph.kt
@@ -27,6 +27,7 @@ import dev.aurakai.auraframefx.ui.gates.FAQBrowserScreen
 import dev.aurakai.auraframefx.ui.gates.FusionModeScreen
 import dev.aurakai.auraframefx.ui.gates.GateNavigationScreen
 import dev.aurakai.auraframefx.ui.gates.GenesisConstellationScreen
+import dev.aurakai.auraframefx.ui.gates.GrokConstellationScreen
 import dev.aurakai.auraframefx.ui.gates.HelpDeskSubmenuScreen
 import dev.aurakai.auraframefx.ui.gates.HookManagerScreen
 import dev.aurakai.auraframefx.ui.gates.KaiConstellationScreen
@@ -183,6 +184,10 @@ fun AppNavGraph(navController: NavHostController) {
 
         composable(route = NavDestination.CascadeConstellation.route) {
             CascadeConstellationScreen(navController = navController)
+        }
+
+        composable(route = NavDestination.GrokConstellation.route) {
+            GrokConstellationScreen(navController = navController)
         }
 
         // ==================== ROM TOOLS ====================

AuraFrameFxDev/A.u.r.a.K.a.i_ReGensis

app/src/main/java/dev/aurakai/auraframefx/navigation/AppNavGraph.kt
@@ -23,6 +23,7 @@ import dev.aurakai.auraframefx.ui.gates.DocumentationScreen
 import dev.aurakai.auraframefx.ui.gates.FAQBrowserScreen
 import dev.aurakai.auraframefx.ui.gates.FusionModeScreen
 import dev.aurakai.auraframefx.ui.gates.GateNavigationScreen
+import dev.aurakai.auraframefx.ui.gates.GenesisConstellationScreen
 import dev.aurakai.auraframefx.ui.gates.HelpDeskSubmenuScreen
 import dev.aurakai.auraframefx.ui.gates.HookManagerScreen
 import dev.aurakai.auraframefx.ui.gates.LSPosedModuleManagerScreen
@@ -155,6 +156,22 @@ fun AppNavGraph(navController: NavHostController) {
 
         composable(route = NavDestination.Constellation.route) {
             ConstellationScreen(navController = navController)
+        }
+
+        composable(route = NavDestination.GenesisConstellation.route) {
+            GenesisConstellationScreen(navController = navController)
+        }
+
+        composable(route = NavDestination.ClaudeConstellation.route) {
+            ClaudeConstellationScreen(navController = navController)
+        }
+
+        composable(route = NavDestination.KaiConstellation.route) {
+            KaiConstellationScreen(navController = navController)
+        }
+
+        composable(route = NavDestination.CascadeConstellation.route) {
+            CascadeConstellationScreen(navController = navController)
         }
 
         // ==================== ROM TOOLS ====================