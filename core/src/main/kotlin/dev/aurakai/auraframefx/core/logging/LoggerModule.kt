0302b559a7c3f8349bbdec872d579b6c5e741e7a
AuraFrame <auraframefx@gmail.com>
2026-05-27 17:14:55 -0600

    refactor: relocate logging utilities to core package
    - Update imports for `AuraFxLogger` and `LogLevel` from `dev.aurakai.auraframefx.domains.cascade.utils` to `dev.aurakai.auraframefx.core.logging`.
    - Update references in `AndroidAuraFxLogger.kt` and `AuraForgeGenerator.kt`.

--- core/src/main/kotlin/dev/aurakai/auraframefx/core/logging/LoggerModule.kt
+++ core/src/main/kotlin/dev/aurakai/auraframefx/core/logging/LoggerModule.kt
@@ -4,7 +4,6 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import dev.aurakai.auraframefx.domains.cascade.utils.AuraFxLogger
 import javax.inject.Singleton
 
 /**
